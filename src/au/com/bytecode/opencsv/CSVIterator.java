a9f7fce9380a13db3196ea6c96dfeceb88d95edb
Hobson, Tanner C <hobsontc@ornl.gov>
2015-07-21 11:27:31 -0400

    Cleaned up JavaDoc comments for CSVIterator

--- src/au/com/bytecode/opencsv/CSVIterator.java
+++ src/au/com/bytecode/opencsv/CSVIterator.java
@@ -7,15 +7,34 @@ public class CSVIterator implements Iterator<String[]> {
     private CSVReader reader;
     private String[] nextLine;
 
+    /**
+     * Create a new iterator to read through each row of a CSV file.
+     *
+     * @param reader The reader to iterate through
+     * @throws IOException if there was an error reading the first line
+     */
     public CSVIterator(CSVReader reader) throws IOException {
         this.reader = reader;
         nextLine = reader.readNext();
     }
 
+    /**
+     * Determine whether there is more data to read.
+     *
+     * @return true if there is more data
+     * @see java.util.Iterator#hasNext
+     */
     public boolean hasNext() {
         return nextLine != null;
     }
 
+    /**
+     * Get the next row of data.
+     *
+     * @return The next row of data
+     * @see java.util.Iterator#next
+     * @see CSVReader#readNext
+     */
     public String[] next() {
         String[] temp = nextLine;
         try {
@@ -26,6 +45,11 @@ public class CSVIterator implements Iterator<String[]> {
         return temp;
     }
 
+    /**
+     * Unimplemented method.
+     *
+     * @see java.util.Iterator#remove
+     */
     public void remove() {
         throw new UnsupportedOperationException("This is a read only iterator.");
     }
